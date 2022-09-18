bb3f5a0cac0231b979b2737a5d8557441a044a09
Rascal_Two <therealrascaltwo@gmail.com>
2022-09-17 21:24:39 -0400

    Add feed sorting

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,6 +4,24 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 const Like = require("../models/Like");
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -16,9 +34,14 @@ module.exports = {
   },
   getFeed: async (req, res) => {
     try {
+      const sortBy = req.query.sort || 'createdAt'
       const page = +req.query.page || 0;
       const start = page * 10;
-      const posts = (await Post.find().populate(['owners', 'likes'])).sort((a, b) => b.createdAt - a.createdAt).slice(start, start + 10);
+      const allPosts = await Post.find().populate(['owners', 'likes'])
+      const posts = (sortBy === 'random'
+        ? shuffle(allPosts)
+        : allPosts.sort((a, b) => b[sortBy] - a[sortBy])
+      ).slice(start, start + 10);
       const lastPage = Math.floor(await Post.count() / 10)
       res.render("feed.ejs", { posts: posts, page, lastPage, dateFns });
     } catch (err) {
@@ -27,7 +50,7 @@ module.exports = {
   },
   getPost: async (req, res) => {
     try {
-      const post = await Post.findById(req.params.id).populate('likes');
+      const post = await Post.findById(req.params.id).populate(['owners', 'likes']);
       res.render("post.ejs", { post: post });
     } catch (err) {
       console.log(err);
