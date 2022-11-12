003c33ea60de191255a5b4e607192d83cf40dae4
Rascal_Two <therealrascaltwo@gmail.com>
2022-11-12 15:49:07 -0500

    Connect to database before starting server

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ require("dotenv").config({ path: "./config/.env" });
 // Passport config
 require("./config/passport")(passport);
 
-//Connect To Database
-connectDB();
+app.set("trust proxy", true);
 
 //Using EJS for views
 app.set("view engine", "ejs");
@@ -36,33 +35,38 @@ app.use(logger("dev"));
 //Use forms for put / delete
 app.use(methodOverride("_method"));
 
-// Setup Sessions - stored in MongoDB
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
+
+//Connect To Database
+connectDB().then(() => {
+
+  // Setup Sessions - stored in MongoDB
+  app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection })
+    })
+  );
+
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(function addUserToViewLocals(request, response, next) {
+    response.locals.user = request.user
+    next();
   })
-);
 
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+  //Use flash messages for errors, info, ect...
+  app.use(flash());
 
-app.use(function addUserToViewLocals(request, response, next){
-  response.locals.user = request.user
-  next();
-})
+  //Setup Routes For Which The Server Is Listening
+  app.use("/", mainRoutes);
+  app.use("/post", postRoutes);
 
-//Use flash messages for errors, info, ect...
-app.use(flash());
-
-//Setup Routes For Which The Server Is Listening
-app.use("/", mainRoutes);
-app.use("/post", postRoutes);
-
-//Server Running
-app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+  //Server Running
+  app.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+  });
 });
